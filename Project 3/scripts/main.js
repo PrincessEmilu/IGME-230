29f74263d34f4d6c80e557a363a72488109aaf69
Emily Horton <emilyhorton1994@gmail.com>
2019-04-18 00:06:08 -0400

    started work on layout; doing css instead this time

--- Project 3/scripts/main.js
+++ Project 3/scripts/main.js
@@ -2,6 +2,26 @@
 
 console.log("Boop!");
 
-//Create the PIXI application
-const app = new PIXI.Application(600,600);
-document.querySelector("#gameSpace").appendChild(app.view); 
+//Declare variables
+let nutrients,nutrientsPerClick,protein,ticks,hosts;
+
+//Grab references to HTML elements
+const gameWindow = document.querySelector("#gameWindow")
+const buttonHarvest = document.querySelector("#harvest");
+const displayStats = document.querySelector("#stats");
+
+
+//Init variables
+nutrients = 0;
+nutrientsPerClick = 1;
+
+//Add events to buttons
+buttonHarvest.onclick = e=>{
+    nutrients += nutrientsPerClick
+    UpdateStatsDisplay();
+};
+
+//Update HUD
+function UpdateStatsDisplay(){
+    displayStats.innerHTML = `Nutrients: ${nutrients}`;
+}
