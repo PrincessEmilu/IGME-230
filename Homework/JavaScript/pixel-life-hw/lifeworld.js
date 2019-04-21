dfd7c7780057b8ab8e0415c6da894d02dcef3ae5
Emily Horton <emilyhorton1994@gmail.com>
2019-04-21 18:19:19 -0400

    Worked on loop; might give up

--- Homework/JavaScript/pixel-life-hw/lifeworld.js
+++ Homework/JavaScript/pixel-life-hw/lifeworld.js
@@ -30,19 +30,66 @@ const lifeworld = {
     },
 
     getLivingNeighbors(row,col){
-		// TODO:
-		// row and col should > than 0, if not return 0
+        // row and col should > than 0, if not return 0
+        if(row <= 0 || col <= 0){
+            return 0;
+        }
 		
-		// row and col should be < the length of the applicable array, minus 1. If not return 0
+        // row and col should be < the length of the applicable array, minus 1. If not return 0
+        if(row > this.world[0].length - 1 || col > this.world[0][0].length - 1){
+            return 0;
+        }
 		
-		// count up how many neighbors are alive at N,NE,E,SE,S,SW,W,SE - use this.world[row][col-1] etc
+        // count up how many neighbors are alive at N,NE,E,SE,S,SW,W,SE - use this.world[row][col-1] etc
+        let sum = 0;
+
+        if(this.world[row -1][col] == 1){
+            sum += 1;
+        }
+        if(this.world[row +1][col] == 1){
+            sum += 1;
+        }
+        if(this.world[row -1][col + 1] == 1){
+            sum += 1;
+        }
+        if(this.world[row +1][col + 1] == 1){
+            sum += 1;
+        }
+        if(this.world[row -1][col - 1] == 1){
+            sum += 1;
+        }
+        if(this.world[row +1][col - 1] == 1){
+            sum += 1;
+        }
+        if(this.world[row][col - 1] == 1){
+            sum += 1;
+        }
+        if(this.world[row][col + 1] == 1){
+            sum += 1;
+        }
 		
-		// return that sum
+        // return that sum
+        return sum;
 	},
 
     step(){
         // TODO:
-        
+        for(let row = 0; row < this.numRows; row++){
+            for(let col = 0; col < this.numCols; col++){
+                let livingNeighbors = this.getLivingNeighbors(row,col);
+
+                //Cell is alive- does it die?
+                if(this.world[row][col] == 1){
+                    if(livingNeighbors < 2 || livingNeighbors > 3){
+                        this.world[row][col] = 0;
+                    }
+                }
+                //Cell is dead- does it become alive?
+                else if(livingNeighbors == 3){
+                    this.world[row][col];
+                }
+            }
+        }
         // nested for loop will call getLivingNeighbors() on each cell in this.world
         // Determine if that cell in the next generation should be alive (see wikipedia life page linked at top)
         // Put a 1 or zero into the right location in this.worldBuffer
