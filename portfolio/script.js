07bdb737a4773f94e99e0a80267098d2758439c9
Emily Horton <emilyhorton1994@gmail.com>
2019-05-02 02:28:49 -0400

    Better script handling of data.

--- portfolio/script.js
+++ portfolio/script.js
@@ -5,7 +5,7 @@ let galleryItems;
 
 //When the window loads, set up event handling
 window.onload = (e) =>{
-    galleryItems = document.querySelectorAll(".gallery-item");
+    galleryItems = document.querySelectorAll(".gallery-item img");
     galleryItems[0].onclick = showHide;
     galleryItems[1].onclick = showHide;
     galleryItems[2].onclick = showHide;
@@ -14,7 +14,7 @@ window.onload = (e) =>{
 
 //Simply changes display type
 function showHide(e){
-    let description = document.querySelector(`#description-${e.target.id}`);
+    let description = document.querySelector(`#description-${e.target.dataset.number}`);
     if(description.style.display != "block") {description.style.display = "block";}
     else {description.style.display = "none";}
 }
